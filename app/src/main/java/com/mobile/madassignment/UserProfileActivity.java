3652aa77d5408cfdd873fc9edc00afcaf3a29096
John Titor <975534268@qq.com>
2017-05-13 05:53:46 +0800

    photo choose from gallery

--- app/src/main/java/com/mobile/madassignment/UserProfileActivity.java
+++ app/src/main/java/com/mobile/madassignment/UserProfileActivity.java
@@ -1,13 +1,16 @@
 package com.mobile.madassignment;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.Handler;
 import android.os.Message;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.text.TextUtils;
 import android.util.Log;
@@ -19,6 +22,8 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -26,7 +31,14 @@ import com.google.firebase.auth.UserProfileChangeRequest;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.storage.FileDownloadTask;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.io.InputStream;
@@ -35,6 +47,8 @@ import java.net.URL;
 
 public class UserProfileActivity extends Activity {
 
+    private Button gallery;
+    private Button camera;
     private Button update_picture;
     private Button update_profile;
     private ImageView back;
@@ -50,6 +64,14 @@ public class UserProfileActivity extends Activity {
     private FirebaseUser user;
     private DatabaseReference mRootRef;
 
+    private static final int PHOTO_REQUEST_CAREMA = 1;
+    private static final int PHOTO_REQUEST_GALLERY = 2;
+    private static final int PHOTO_REQUEST_CUT = 3;
+    private Bitmap tempPic;
+
+    private static final String PHOTO_FILE_NAME = "temp_photo.jpg";
+    private File tempFile;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -59,6 +81,8 @@ public class UserProfileActivity extends Activity {
         mAuth = FirebaseAuth.getInstance();
         user = mAuth.getCurrentUser();
 
+        gallery = (Button)findViewById(R.id.photo_from_gallery);
+        camera = (Button)findViewById(R.id.photo_from_camera);
         update_picture = (Button)findViewById(R.id.bt_update_picture);
         update_profile = (Button)findViewById(R.id.bt_update_profile);
         back = (ImageView)findViewById(R.id.iv_user_profile_back);
@@ -66,7 +90,6 @@ public class UserProfileActivity extends Activity {
         password = (EditText)findViewById(R.id.user_password);
         repeatPW = (EditText)findViewById(R.id.user_password_repeat);
         picture = (ImageView)findViewById(R.id.picture);
-        picture_url = (EditText)findViewById(R.id.picture_url);
         emailAddress = (TextView)findViewById(R.id.user_email);
         change_password = (Button)findViewById(R.id.bt_change_paasword);
 
@@ -79,15 +102,50 @@ public class UserProfileActivity extends Activity {
 
         final String name = user.getDisplayName();
         String emailstr = user.getEmail();
-        Uri photo = user.getPhotoUrl();
+        //Uri photo = user.getPhotoUrl();
 
         //Toast.makeText(UserProfileActivity.this, "url:" + photo.toString(),Toast.LENGTH_SHORT ).show();
-        if(photo != null) {
-            DownImage PrintImage = new DownImage(picture, photo.toString());
-            PrintImage.execute();
+        FirebaseStorage storage = FirebaseStorage.getInstance();
+        StorageReference storageRef = storage.getReferenceFromUrl("gs://madassignment-1f6c6.appspot.com");
+        StorageReference photoRef = storageRef.child(user.getUid()+".jpg");
+
+        try {
+            final File localFile = File.createTempFile("images", "jpg");
+            final long ONE_MEGABYTE = 1024*1024;
+            photoRef.getFile(localFile).addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+                    // Local temp file has been created
+                    Picasso.with(UserProfileActivity.this).load(localFile).into(picture);
+                    Toast.makeText(UserProfileActivity.this, "photo download success",Toast.LENGTH_SHORT ).show();
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception exception) {
+                    // Handle any errors
+                    Toast.makeText(UserProfileActivity.this, "photo download failed",Toast.LENGTH_SHORT ).show();
+                }
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+
+
         nickname.setText(name);
         emailAddress.setText(emailstr);
+
+        gallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                on_gallery_click();
+            }
+        });
+        camera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                on_camera_click();
+            }
+        });
 
         update_profile.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -105,11 +163,11 @@ public class UserProfileActivity extends Activity {
                                 public void onComplete(@NonNull Task<Void> task) {
                                     if (task.isSuccessful()) {
                                         Log.v("update profile", "User profile updated.");
-                                        Toast.makeText(UserProfileActivity.this, "profile update success.",Toast.LENGTH_SHORT ).show();
+                                        //Toast.makeText(UserProfileActivity.this, "profile update success.",Toast.LENGTH_SHORT ).show();
                                         UserProfileActivity.this.finish();
                                     }
                                     else {
-                                        Toast.makeText(UserProfileActivity.this, "profile update failed.",Toast.LENGTH_SHORT ).show();
+                                        //Toast.makeText(UserProfileActivity.this, "profile update failed.",Toast.LENGTH_SHORT ).show();
                                     }
                                 }
                             });
@@ -120,24 +178,29 @@ public class UserProfileActivity extends Activity {
         update_picture.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String urlStr = picture_url.getText().toString();
-                UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
-                        .setPhotoUri(Uri.parse(urlStr))
-                        .build();
-                user.updateProfile(profileUpdates)
-                        .addOnCompleteListener(new OnCompleteListener<Void>() {
-                            @Override
-                            public void onComplete(@NonNull Task<Void> task) {
-                                if (task.isSuccessful()) {
-                                    Log.v("update profile", "User picture updated.");
-                                    Toast.makeText(UserProfileActivity.this, "picture update success.",Toast.LENGTH_SHORT ).show();
-                                    UserProfileActivity.this.finish();
-                                }
-                                else {
-                                    Toast.makeText(UserProfileActivity.this, "picture update failed.",Toast.LENGTH_SHORT ).show();
-                                }
-                            }
-                        });
+                picture.setDrawingCacheEnabled(true);
+                picture.buildDrawingCache();
+                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                tempPic.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+                byte[] data = baos.toByteArray();
+
+                FirebaseStorage storage = FirebaseStorage.getInstance();
+                StorageReference storageRef = storage.getReferenceFromUrl("gs://madassignment-1f6c6.appspot.com");
+                StorageReference photoRef = storageRef.child(user.getUid()+".jpg");
+                UploadTask uploadTask = photoRef.putBytes(data);
+                uploadTask.addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception exception) {
+                        Toast.makeText(UserProfileActivity.this, "Photo update failed.",Toast.LENGTH_SHORT ).show();
+                    }
+                }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        // taskSnapshot.getMetadata() contains file metadata such as size, content-type, and download URL.
+                        Toast.makeText(UserProfileActivity.this, "Photo update success.",Toast.LENGTH_SHORT ).show();
+                        //Uri downloadUrl = taskSnapshot.getDownloadUrl();
+                    }
+                });
             }
         });
 
@@ -190,27 +253,75 @@ public class UserProfileActivity extends Activity {
         });
     }
 
+    public void on_gallery_click() {
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        startActivityForResult(intent, PHOTO_REQUEST_GALLERY);
+    }
 
-//    public Bitmap getURLimage(String url) {
-//        Bitmap bmp = null;
-//        try {
-//            URL myurl = new URL(url);
-//            HttpURLConnection conn = (HttpURLConnection) myurl.openConnection();
-//            conn.setConnectTimeout(6000);
-//            conn.setDoInput(true);
-//            conn.setUseCaches(false);
-//            conn.connect();
-//            InputStream is = conn.getInputStream();
-//            bmp = BitmapFactory.decodeStream(is);
-//            is.close();
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//        return bmp;
-//    }
+    public void on_camera_click() {
+        Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+        if (hasSdcard()) {
+            tempFile = new File(Environment.getExternalStorageDirectory(),
+                    PHOTO_FILE_NAME);
+            Uri uri = Uri.fromFile(tempFile);
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, uri);
+        }
+        startActivityForResult(intent, PHOTO_REQUEST_CAREMA);
+    }
+    private void crop(Uri uri) {
+        Intent intent = new Intent("com.android.camera.action.CROP");
+        intent.setDataAndType(uri, "image/*");
+        intent.putExtra("crop", "true");
+        // 1:1
+        intent.putExtra("aspectX", 1);
+        intent.putExtra("aspectY", 1);
+        //size
+        intent.putExtra("outputX", 125);
+        intent.putExtra("outputY", 140);
 
+        intent.putExtra("outputFormat", "JPEG");
+        intent.putExtra("noFaceDetection", true);
+        intent.putExtra("return-data", true);
+        startActivityForResult(intent, PHOTO_REQUEST_CUT);
+    }
 
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == PHOTO_REQUEST_GALLERY) {
+            if (data != null) {
+                Uri uri = data.getData();
+                crop(uri);
+            }
+        } else if (requestCode == PHOTO_REQUEST_CAREMA) {
+            if (hasSdcard()) {
+                crop(Uri.fromFile(tempFile));
+            } else {
+                Toast.makeText(UserProfileActivity.this, "SDCard nod found,no space for photo", Toast.LENGTH_SHORT ).show();
+            }
 
+        } else if (requestCode == PHOTO_REQUEST_CUT) {
+            if (data != null) {
+                tempPic = data.getParcelableExtra("data");
+                picture.setImageBitmap(tempPic);
+            }
+            try {
+                tempFile.delete();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private boolean hasSdcard() {
+        if (Environment.getExternalStorageState().equals(
+                Environment.MEDIA_MOUNTED)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
     private boolean isPasswordValid(String password) {
         return password.length() >= 6;
@@ -218,31 +329,5 @@ public class UserProfileActivity extends Activity {
 
     private boolean isPasswordRepeatedCorrectly(String password, String repeat) {
         return password.equals(repeat);
-    }
-
-}
-
-class DownImage extends AsyncTask {
-    private ImageView imageView;
-    private String url;
-    public DownImage(ImageView imageView, String url) {
-        this.imageView = imageView;
-        this.url = url;
-    }
-
-    protected void onPostExecute(Object result) {
-        imageView.setImageBitmap((Bitmap)result);
-    }
-
-    @Override
-    protected Object doInBackground(Object[] objects) {
-        Bitmap bitmap = null;
-        try {
-            InputStream is = new URL(url).openStream();
-            bitmap = BitmapFactory.decodeStream(is);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return bitmap;
     }
 }
